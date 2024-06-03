de58483eb4f2b8a00b5c449be5bc777c3b814aab
Karen-Nunes <knnicke12@gmail.com>
2024-06-03 19:51:01 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//entrada
+let heroi = "Batman";
+let xp = 1001;
+
+switch (xp) {
+    case 0 : 1000
+    console.log("Ferro");
+    break;
+    case 1001 : 2000
+    console.log("Bronze");
+    break;
+    case 2001 : 5000
+    console.log("Prata");
+    break;
+    case 5001 : 7000
+    console.log("Ouro");
+    break;
+    case 7001 : 8000
+    console.log("Platina");
+    break;
+    case 8001 : 9000
+    console.log("Ascendente")
+    break;
+    case 9001 : 10000
+    console.log("Imortal")
+    default:
+    console.log("Radiante");
+  }
+
+//saída
+console.log ("O herói de nome " + heroi + " está no nível " + xp)
